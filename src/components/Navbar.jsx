6c882d890003549310dc1b64afa2709697657660
Sinclair <sinclairolajuwon@gmail.com>
2024-09-24 17:19:15 -0500

    learning react on my own

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import logo from "../assets/react.svg"
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between bg-[#21222A]'>
-        <div className='flex space-x-2 items-center ml-3 mt-3 text-xl mb-5'>
+        <div className='flex space-x-2 items-center ml-3 mt-6 text-xl mb-5'>
         <img src={logo} alt="" />
         <h1 className='font-inter font-extrabold text-[#61DAFB]'>ReactFacts</h1>
     </div>
-    <div className='mt-3 mr-3 mb-5'>
+    <div className='mt-6 mr-3 mb-5'>
         <h1 className='font-inter font-bold text-xl text-white'>
             React Course - Project 1
         </h1>
